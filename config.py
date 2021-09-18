ee3c7399c2107a4523fee0b15feb8f0887e05bbd
shubham-king <84066395+shubham-king@users.noreply.github.com>
2021-09-18 08:11:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
-
+BOT_NAME = getenv("BOT_NAME", "Music v1.2)
+BOT_USERNAME = getenv("BOT_USERNAME")
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
