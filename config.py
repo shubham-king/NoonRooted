993f12c5680ffffb587c06f5b78e11f2fd6a253f
shubham-king <84066395+shubham-king@users.noreply.github.com>
2021-09-19 13:09:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,6 +11,8 @@ BOT_USERNAME = getenv("BOT_USERNAME")
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
+PM_PERMIT = getenv("PM_PERMIT", "ENABLE")
+
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
